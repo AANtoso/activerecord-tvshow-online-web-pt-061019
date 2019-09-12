b675e8693c3db5c1c40b55e4437147019d530b03
github username <github email address>
2019-09-12 11:48:23 -0400

    Done.

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,8 +1,10 @@
 class CreateShows < ActiveRecord::Migration[5.2]
-    create_table :shows do |t|
-        t.string :name
-        t.string :day
+    def change
+      create_table :shows do |t|
+        t.string   :name
         t.string :network
-        t.integer :rating
+        t.string  :day
+        t.integer  :rating
+      end
     end
-end
+  end
